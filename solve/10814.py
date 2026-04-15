25df84512445c76b5160664a4af87e06deaf736b
yunyoungyee <eric3725@naver.com>
2026-04-15 14:06:59 +0900

    기본 : key 값으로만 정렬

--- solve/10814.py
+++ solve/10814.py
@@ -1,11 +1,10 @@
+# sort(key)에다가 정렬 기준을 lambda를 써서 x값으로 정렬하자는 건데,
+# 그걸 x[0]으로 한 이유는 key값으로 정렬을 하고 싶은데 튜플이라서 저렇게 접근하는 것임.
 N = int(input())
 member=[]
 for _ in range(N):
     k, v = input().split()
-    member.append((k,v))
+    member.append((int(k),v))
 member.sort(key=lambda x: x[0])
 for k, v in member:
     print(k, v)
-# member={}
-# for _ in range(N):
-#     member
